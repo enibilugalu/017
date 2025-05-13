4f54d19a501de0ba50c42ea0809fc609975b7439
enibilugalu <belskijivan44@gmail.com>
2025-05-13 21:55:31 +0300

    second commit

--- file2.cpp
+++ file2.cpp
@@ -1,27 +1,37 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-int main() 
+int main()
 {
     setlocale(LC_ALL, "Russian");
 
-    string input;
+    char input[256];
     char insertChar;
 
     cout << "Введите предложение: ";
-    getline(cin, input);
+    cin.getline(input, 256);
     cout << "Введите символ для вставки: ";
     cin >> insertChar;
-    string result;
-    for (size_t i = 0; i < input.size(); i++) 
+
+    char result[512];
+    int j = 0;
+
+    int length = 0;
+    while (input[length] != '\0')
     {
-        result += input[i];
-        if ((i + 1) % 2 == 0) 
+        length++;
+    }
+
+    for (int i = 0; i < length; i++)
+    {
+        result[j++] = input[i];
+        if ((i + 1) % 2 == 0)
         {
-            result += insertChar;
+            result[j++] = insertChar;
         }
     }
+    result[j] = '\0';
+
     cout << "Измененное предложение: " << result << endl;
 
     return 0;
